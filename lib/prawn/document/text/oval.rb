5f4b65e1621048765b6ead88b8a66084dc62edd4
Daniel Nelson <daniel@Optiplex760.(none)>
2009-10-26 14:04:49 -0500

    Readme and comments

--- lib/prawn/document/text/oval.rb
+++ lib/prawn/document/text/oval.rb
@@ -2,7 +2,7 @@
 
 # text/oval.rb : Implements simple text ovals
 #
-# Contributed by Daniel Nelson. October, 2009
+# Copyright October 2009, Daniel Nelson. All Rights Reserved.
 #
 # This is free software. Please see the LICENSE and COPYING files for details.
 
@@ -12,16 +12,34 @@ module Prawn
       # Draws the requested text into an oval. When the text overflows
       # the oval, you can either display :ellipses or :truncate the text
       #   acceptable options:
-      #       :width
-      #       :height
-      #       :at => upper left corner of the bounding box around the ellipse
-      #       :center => center of the ellipse (overrides :at)
-      #       :overflow => :truncate or :ellipses. defaults to :truncate
-      #       :crop => space between top of the oval and the first line.
-      #                defaults to 0
-      #       :leading => ie: @document.font.height * 0.5, defaults to 0
-      #       :kerning => true or false. defaults to false
-      #       :align => :center, :left, or :right. defaults to :center
+      #
+      #     :width and :height are the width and height of the
+      #       ellipse, respectively. If they are equal, then the shape
+      #       is a circle.
+      #
+      #     :at is a two element array denoting the upper left corner
+      #       of the bounding box around the ellipse
+      #
+      #     :center is a two element array denoting the center of the
+      #       ellipse. If :center is present, then its value overrides
+      #       that provided by :at
+      #
+      #     :overflow is either :truncate or :ellipses, denoting the
+      #       behavior when the amount of text exceeds the available
+      #       space. Defaults to :truncate.
+      #
+      #     :crop denotes the space between top of the oval and the
+      #       first line. It is used to push the starting line lower in
+      #       the circle when the combination of oval size and font size
+      #       result in only a few characters being displayable on the
+      #       very top line.
+      #
+      #     :leading is the amount of space between lines. Defaults to 0
+      #
+      #     :kerning is a boolean. Defaults to false. Note that a
+      #       value of true will result in slower width computations
+      #   
+      #     :align is :center, :left, or :right. Defaults to :center
 
       # +string+ must be UTF8-encoded.
       #
