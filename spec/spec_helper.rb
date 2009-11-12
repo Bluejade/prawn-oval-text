b8d03a5b836ea0374266ce84bdbe015a1f705c50
Daniel Nelson <daniel@Optiplex760.(none)>
2009-11-12 16:19:04 -0600

    Now that Prawn 0.6 is out make specs and example use Prawn gem instead of vendor hack

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,7 @@
 # encoding: utf-8
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
-prawn_symlink = File.join(File.dirname(__FILE__), '..', 'vendor','prawn')
-unless File.symlink?(prawn_symlink)
-  puts 'Running specs requires symlinked vendor/prawn directory that points to prawn'
-  return
-end
-prawn_dir = File.readlink(prawn_symlink)
-$LOAD_PATH.unshift File.join(prawn_dir, 'lib')
-
+require 'rubygems'
 require 'prawn/core'
 Prawn.debug = true
 
